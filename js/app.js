7599c8cecc5c6fb95e593919712be6d7275197e4
Briana Wright <brianaleew@gmail.com>
2023-01-03 11:20:29 -0500

    restructured canvas/play area for game. defined elements in js needed for mvp

--- js/app.js
+++ js/app.js
@@ -1,1 +1,18 @@
-console.log("testing")
+//console.log("testing")
+
+// //////////////MVP Requirements ////////////////
+
+// player should be able to click the fight button to begin the game (more button options later)
+// player should be able to move their heart icon with W,A,S,D
+// Lose health when contact is made by the heart icon and a projectile
+//Display a Winner message after 20 projectiles have been dodged successfully
+
+
+
+//step one: grab all elements we need 
+
+const gameplayArea = document.getElementById('canvas')
+const fightBtn = document.getElementById('fight-button')
+const resetBtn = document.getElementById('reset-button')
+const playerHealth = document.getElementById('health-bar')
+const gameText = document.getElementById('player-message')
