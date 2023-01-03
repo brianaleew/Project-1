ff5e199f561ff20aff87fedd33961373616d0d6c
Briana Wright <brianaleew@gmail.com>
2023-01-03 14:38:22 -0500

    set up a projectile class that draws circles. started setting up fight button

--- js/app.js
+++ js/app.js
@@ -23,37 +23,16 @@ const ctx = gameplayArea.getContext('2d')
 
 gameplayArea.setAttribute('width', getComputedStyle(gameplayArea)['width'])
 gameplayArea.setAttribute('height', getComputedStyle(gameplayArea)['height'])
-// Getting Image
 
+                // Set up Fight Button
 
+   const beginFight = () => {
+        const projectiles = new 
 
-// Player Icon 
-
-// const player = {
-//     x: 20,
-//     y: 20,
-//     width: 50,
-//     height: 50, 
-//     alive: true, 
-//     render: function () {
-        
-//         let img = new Image()
-// img.src = 'Images/8bit heart (project 1).png'
-
-
-//         fill_gameplayArea(img);
-//         ctx.drawImage(img,this.x, this.y)
-
-
-//     }
-
-
-// }
-
-// player.render()
-
-
-
+   }
+   
+   
+                fightBtn.addEventListener('click', beginFight)
 // gameMessage.innerText = "click fight to begin"
 // gameMessage.style.color = 'pink'
 
@@ -72,6 +51,42 @@ window.onload = function () {
     function fill_canvas(img) {
 
         ctx.drawImage(img, 120, 200, 50, 50); 
-              
+
     }
 }
+
+
+// Trying to create Projectiles for player to dodge
+
+class projectile {
+    constructor (x, y, width, height, radius, velocity) {
+        this.x = x
+        this.y = y 
+        this.width = width 
+        this.height = height 
+        this.radius = radius 
+        this.velocity = velocity 
+        this.color = 'black' 
+       
+    }
+    draw() {
+        ctx.beginPath()
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI *2, false)
+        ctx.fillStyle = this.color
+        ctx.fill()
+    }
+   
+}
+
+const projectileUno = new projectile(123, 123, 123, 123, 123, 123)
+
+
+
+
+// window.addEventListener('click', () => {
+//     console.log('yerrrr') 
+// })
+
+
+
+
